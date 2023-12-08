361282f86f2655cbbb0114a72bb6fbfb980ad250
Chris <hereokay88@gmail.com>
2023-12-08 20:45:58 +0900

    feat: getEthereumPriceOndate and updateSpendGasUSDTInMongoDB

--- index.js
+++ index.js
@@ -1,15 +1,98 @@
 import { BigQuery } from '@google-cloud/bigquery';
 import fs from 'fs';
 import path from 'path';
+const axios = require('axios');
+const { MongoClient } = require('mongodb');
 
+/*
+시나리오 1 : Bigquery 연결 실패
+시나리오 2 : coingecko API 연결 실패
+시나리오 3 : 
 
+*/
+const mongoUrl = 'your-mongodb-connection-string';
+const dbName = 'your-db-name';
+const collectionName = 'users';
 
-function processDataAndSave(date){
-  rows = fetchQueryData(date);
-  console.log(rows);
+function convertDateFormat(date) {
+  const parts = date.split("-");
+  if (parts.length !== 3) {
+      throw new Error("Invalid date format");
+  }
+  return `${parts[2]}-${parts[1]}-${parts[0]}`;
 }
 
-const { BigQuery } = require('@google-cloud/bigquery');
+
+
+async function processDataAndSave(date){
+  let rows;
+  let ethPrice;
+
+  try {
+    rows = await fetchQueryData(date);
+  } catch (error) {
+    console.error('Error fetching query data:', error);
+    throw new Error(`fetchQueryData failed for date: ${date}`); // 또는 다른 적절한 에러 처리
+  }
+
+  try {
+    ethPrice = await getEthereumPriceOnDate(date);
+  } catch (error) {
+    console.error(`Error fetching Ethereum price for date: ${date}`, error);
+    throw new Error(`getEthereumPriceOnDate failed for date: ${date}`);
+  }
+
+  try {
+    await updateSpendGasUSDTInMongoDB(mongoUrl, dbName, collectionName, rows, ethPrice);
+  } catch (err) {
+    console.error('Error:', err);
+    throw new Error(`updateSpendGasUSDTInMongoDB failed for date: ${date}`);
+  }
+  
+}
+
+async function updateSpendGasUSDTInMongoDB(mongoUrl, dbName, collectionName, rows, ethPrice) {
+  const client = new MongoClient(mongoUrl);
+
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+
+    for (const row of rows) {
+      const _id = row._id;
+      const gasCost = row.gasCost;
+      const spendGasUSDT = gasCost * ethPrice;
+
+      const updateResult = await collection.updateOne(
+        { _id: _id },
+        { $inc: { spendGasUSDT: spendGasUSDT } },
+        { upsert: true }
+      );
+
+      console.log(`_id ${_id}: Document updated: ${updateResult.modifiedCount}, Document inserted: ${updateResult.upsertedCount}`);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    await client.close();
+  }
+}
+
+async function getEthereumPriceOnDate(date) {
+  try {
+    const url = `https://api.coingecko.com/api/v3/coins/ethereum/history?date=${convertDateFormat(date)}`;
+    const response = await axios.get(url);
+    const price = response.data.market_data.current_price.usd;
+    return price;
+  } catch (error) {
+    console.error(`Error fetching Ethereum price for date: ${date}`, error);
+    throw error;
+  }
+}
+
 
 const bigqueryClient = new BigQuery();
 
